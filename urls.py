efa3bf17672b81cf765838df15723e133a855df5
shalurastogi7 <141400209+shalurastogi7@users.noreply.github.com>
2023-08-26 12:17:43 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,21 +1,22 @@
-from django.urls import path
-from Examapp.views import *
-urlpatterns=[
-    path('index',index,name="Homepage"),
-    path('signup',signup,name="signup"),
-    path('login',loginup,name="login"),
-    path('Tsignup',signUp,name="signup"),
-    path('Tlogin',loginUP,name="loginup"),
-    path('admin',admin,name="admin"),
-    path('contact',contact,name="contact"),
-    path('tdashboard',tdashboard,name="tdashboard"),
-    path('mcourse',mcourse,name="mcourse"),
-    path('mquestion',mquestion,name="mquestion"),
-    path('addexam',addexam,name="addexam"),
-    path('viewexam',viewexam,name="viewexam"),
-    path('viewquestion',viewquestion,name="viewquestion"),
-    path('addquestion',addquestion,name="addquestion"),
+"""Online URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
-    
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('Examapp/',include('Examapp.urls'))
 ]
